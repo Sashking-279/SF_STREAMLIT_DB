0264b62794d0dd994ce49b4adbc18ac1e6b0f7cd
Sashanth <113826069+Sashking-279@users.noreply.github.com>
2024-02-08 15:16:28 +0530

    Update MY_DB_APP.py

--- MY_DB_APP.py
+++ MY_DB_APP.py
@@ -4,3 +4,8 @@ import requests
 from urllib.error import URLError
 
 st.title('SNOWFLAKE DASHBOARDS')
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
