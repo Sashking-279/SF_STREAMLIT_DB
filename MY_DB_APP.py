3161dcc7759705cd094ecaad89973c7e7d2be311
Sashanth <113826069+Sashking-279@users.noreply.github.com>
2024-02-09 11:24:08 +0530

    Update MY_DB_APP.py

--- MY_DB_APP.py
+++ MY_DB_APP.py
@@ -3,6 +3,8 @@ import pandas as pd
 import requests
 import numpy as np
 from urllib.error import URLError
+from snowflake.snowpark import Session
+import snowflake.snowpark as snowpark 
 
 st.title('SNOWFLAKE DASHBOARDS')
 st.write("Here's our first attempt at using data to create a table:")
@@ -17,3 +19,18 @@ if st.checkbox('Show dataframe'):
        columns=['a', 'b', 'c'])
 
     chart_data
+
+connection_parameters = {
+    "account": "wu00137.central-india.azure",
+    "user": "sashanth",
+    "password": "Alpha123@",
+    "role": "ACCOUNTADMIN",  
+    "warehouse": "COMPUTE_WH", 
+    "database": "S_T_VALIDATION", 
+    "schema": "Test", 
+ }  
+
+new_session = Session.builder.configs(connection_parameters).create()
+tableName = 'S_T_VALIDATION.Test.MAPPING_TABLE'
+df_table = new_session.table(tableName)
+df=df_table.to_pandas()
