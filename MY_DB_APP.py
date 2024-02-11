4d688d41a1690fd10adb3534b9f20811bee5de43
Sashanth <113826069+Sashking-279@users.noreply.github.com>
2024-02-12 01:40:10 +0530

    Update MY_DB_APP.py

--- MY_DB_APP.py
+++ MY_DB_APP.py
@@ -18,7 +18,6 @@ conn = st.connection("snowflake")
 	
 
 df1 = conn.query("SHOW ROLES;", ttl=600)
-df1 = conn.query('SELECT "name" as ROLES ,"assigned_to_users" as ASSIGNED_TO_USERS FROM table(result_scan(last_query_id())) WHERE 
-		"assigned_to_users" >= 1; ', ttl=600)
+df1 = conn.query('SELECT "name" as ROLES ,"assigned_to_users" as ASSIGNED_TO_USERS FROM table(result_scan(last_query_id())) WHERE "assigned_to_users" >= 1; ', ttl=600)
 
 st.write(df)
